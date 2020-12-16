1765b8043ae3d8fb82ac2affe8b954f0aacf3a50
Canh <canhmeoladykiler01@gmail.com>
2020-12-16 13:36:27 +0700

    done ui

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+
+const Header = memo(() => {
+  return (
+    <header className='header'>
+      <h1>todos</h1>
+      <input className='new-todo' />
+    </header>
+  );
+});
+
+export default Header;
